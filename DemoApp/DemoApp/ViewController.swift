3e1fa0fffe34e41b3def91d1208636d4d82a58b2
tima <tsimafei@scandit.com>
2023-11-11 20:29:38 +0100

    Project created

---
+++ DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Tima Sikorski on 11/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
